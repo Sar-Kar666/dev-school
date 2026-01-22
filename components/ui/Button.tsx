ce5288c7d49c6e0f18679f7a26dbf5d20c4c4847
Sar-Kar666 <diptanusarkar699@gmail.com>
2026-01-22 20:56:09 +0530

    added a carousel

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,18 +1,22 @@
-
-
 interface ButtonProps {
-  children: React.ReactNode,
-  color : string,
+  children: React.ReactNode;
+  color: string;
   variant?: "primary" | "secondary";
 }
 
 const variants = {
-  primary: "border cursor-pointer px-2 py-1 rounded-md flex  items-center",
-  secondary: "border cursor-pointer px-4 py-2 rounded-md flex  items-center",
-};
+  primary: "border cursor-pointer px-2 py-1 rounded-md flex items-center",
+  secondary: "border cursor-pointer px-4 py-2 rounded-md flex items-center",
+} as const;
 
-export function Button({children,color,variant}: ButtonProps){
-    return<div>
-            <button className={`$${variants[variant]} ${color}`}>{children}</button>
-    </div>
+export function Button({
+  children,
+  color,
+  variant = "primary",
+}: ButtonProps) {
+  return (
+    <button className={`${variants[variant]} ${color}`}>
+      {children}
+    </button>
+  );
 }
